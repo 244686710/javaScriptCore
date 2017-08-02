8330b87ca64db9295f6cf96bd9665a34c9bd6d94
yuyadong <244686710@qq.com>
2017-08-02 18:34:38 +0800

    面向对象学习

--- js/index.js
+++ js/index.js
@@ -15,6 +15,58 @@ Object.defineProperty( person, "name", {
     value: 'Nicholas'
 });
 
-alert(person.name);
-person.name = "Greg";
-alert(person.name);
+// alert(person.name);
+// person.name = "Greg";
+// alert(person.name);
+
+// var book = {
+//     _yaer: 2004,
+//     edition: 1
+// }
+
+// Object.defineProperty(book, "year", {
+//     get: function() {
+//         return this._yaer
+//     },
+//     set: function(newValue) {
+//         if (newValue > 2004) {
+//             this._year = newValue;
+//             this.edition += newValue - 2004;
+//         }
+   
+//     }
+// });
+
+// book.year = 2005;
+// alert(book.edition); //2
+
+
+// 读取属性的特性 Object.getOwnPropertyDescriptor()
+
+var book = {};
+Object.defineProperties(book, {
+    _year: {
+        value: 2004,
+    },
+
+    edition: {
+        value: 1
+    },
+
+    year: {
+        get: function() {
+            return this._year;
+        },
+
+        set: function(newValue) {
+            if(newValue > 2004) {
+                this._year = newValue;
+                this.edition += newValue - 2004;
+            }
+        }
+
+    }
+})
+
+var descriptor = Object.getOwnPropertyDescriptor(book, "_year");
+console.log(descriptor.value);
