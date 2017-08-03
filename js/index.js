7729c9791f25eeca5938ed24c2cdd70f8faa3e5f
yuyadong <244686710@qq.com>
2017-08-03 17:45:27 +0800

    函数表达式，面向对象

--- js/index.js
+++ js/index.js
@@ -43,30 +43,84 @@ Object.defineProperty( person, "name", {
 
 // 读取属性的特性 Object.getOwnPropertyDescriptor()
 
-var book = {};
-Object.defineProperties(book, {
-    _year: {
-        value: 2004,
-    },
+// var book = {};
+// Object.defineProperties(book, {
+//     _year: {
+//         value: 2004,
+//     },
 
-    edition: {
-        value: 1
-    },
+//     edition: {
+//         value: 1
+//     },
 
-    year: {
-        get: function() {
-            return this._year;
-        },
+//     year: {
+//         get: function() {
+//             return this._year;
+//         },
 
-        set: function(newValue) {
-            if(newValue > 2004) {
-                this._year = newValue;
-                this.edition += newValue - 2004;
-            }
-        }
+//         set: function(newValue) {
+//             if(newValue > 2004) {
+//                 this._year = newValue;
+//                 this.edition += newValue - 2004;
+//             }
+//         }
 
+//     }
+// })
+
+// var descriptor = Object.getOwnPropertyDescriptor(book, "_year");
+// console.log(descriptor.value);
+
+// function Person() {
+
+// } ;
+// Person.prototype.name = 'Yadong';
+// Person.prototype.age= 26;
+// Person.prototype.job = "Software Engineer";
+// Person.prototype.sayName = function() {
+//     console.log(this.name);
+// }
+
+// var person1 = new Person();
+// console.log(person1);
+// console.log('Person.prototype.isProtypeOf(person1) =' + Person.prototype.isPrototypeOf(person1));
+// person1.sayName();
+
+// var o = {
+//     toString : function(){
+//     return "My Object";
+// }
+// };
+// for (var prop in o){
+//     if (prop == "toString"){
+//     alert("Found toString"); //在 IE 中不会显示
+//     }
+// }
+
+// // Object.key()
+// var keys = Object.keys(Person.prototype);
+// console.log('Person.prototype key :' + keys)
+
+// 组合使用构造函数模式与原型模式
+function Person(age, name, job) {
+    this.name = name;
+    this.age = age;
+    this.job =job;
+    this.friends = ['Mary', 'Lisa']
+}
+
+Person.prototype = {
+    constructor: Person,
+    sayName: function() {
+        console.log(this.name)
     }
-})
+}
 
-var descriptor = Object.getOwnPropertyDescriptor(book, "_year");
-console.log(descriptor.value);
+var person1 = new Person(26, 'Yadong', 'WEB Engnieer');
+var person2 = new Person(20, 'Mov', 'WEB Engineer');
+
+person1.friends.push('MM');
+
+console.log('person1.friends:'+ person1.friends);
+console.log('person2.friends:'+ person2.friends);
+console.log('Person.friends:'+ Person.friends);
